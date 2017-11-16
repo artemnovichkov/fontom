e63ef7667aa8637ffb3a94067ed8c5020bb26947
Artem Novichkov <artem.novichkov@rosberry.com>
2017-11-16 14:19:01 +0600

    Add Fabric

--- Fontom/AppDelegate.swift
+++ Fontom/AppDelegate.swift
@@ -3,6 +3,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -10,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         return true
     }
 }
